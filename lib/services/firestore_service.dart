3a04c8eefb67bc7ac5aaea95e0021845ea4700a1
Emma Ortiz <168940767+emma-ortiz@users.noreply.github.com>
2026-04-30 13:00:38 -0600

    Refactor FirestoreService to include obtenerCategorias

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,15 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/fruta.dart';
+import '../models/categoria.dart';
 
 class FirestoreService {
   final db = FirebaseFirestore.instance;
 
-  // CREATE
+  // FRUTAS
   Future<void> agregarFruta(Fruta fruta) async {
     await db.collection('frutas').add(fruta.toMap());
   }
 
-  // READ
   Stream<List<Fruta>> obtenerFrutas() {
     return db.collection('frutas').snapshots().map((snapshot) =>
         snapshot.docs
@@ -17,21 +17,19 @@ class FirestoreService {
             .toList());
   }
 
-  // UPDATE
   Future<void> actualizarFruta(Fruta fruta) async {
     await db.collection('frutas').doc(fruta.id).update(fruta.toMap());
   }
 
-  // DELETE
   Future<void> eliminarFruta(String id) async {
     await db.collection('frutas').doc(id).delete();
   }
-}
 
-// OBTENER CATEGORIAS
-Stream<List<Categoria>> obtenerCategorias() {
-  return db.collection('categorias').snapshots().map((snapshot) =>
-      snapshot.docs
-          .map((doc) => Categoria.fromFirestore(doc.id, doc.data()))
-          .toList());
+  // CATEGORIAS
+  Stream<List<Categoria>> obtenerCategorias() {
+    return db.collection('categorias').snapshots().map((snapshot) =>
+        snapshot.docs
+            .map((doc) => Categoria.fromFirestore(doc.id, doc.data()))
+            .toList());
+  }
 }
