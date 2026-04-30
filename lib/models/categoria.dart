10c8cad8e9cc7c328559d7e3e2e2def99514d150
Emma Ortiz <168940767+emma-ortiz@users.noreply.github.com>
2026-04-30 13:00:01 -0600

    Remove toMap method from Categoria model
    Removed toMap method from Categoria class.

--- lib/models/categoria.dart
+++ lib/models/categoria.dart
@@ -4,12 +4,6 @@ class Categoria {
 
   Categoria({required this.id, required this.nombre});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'nombre': nombre,
-    };
-  }
-
   factory Categoria.fromFirestore(String id, Map<String, dynamic> data) {
     return Categoria(
       id: id,
